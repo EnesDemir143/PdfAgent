6bb19a8027ee602771762e918d263c35558a103c
EnesDemir143 <enesd41@outlook.com>
2025-05-12 11:58:45 +0300

    web search tool kısmı ayarlandı.

---
+++ Agent/Tools/Article.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+class Article(BaseModel):
+    title: str
+    source: str
+    link: str
+    snippet: str
+
+    @classmethod
+    def from_serpapi_result(cls, result: dict) -> "Article":
+        return cls(
+            title=result["title"],
+            source=result["source"],
+            link=result["link"],
+            snippet=result["snippet"],
+        )
--- Agent/Tools/Tools.py
+++ Agent/Tools/Tools.py
@@ -1,14 +1,24 @@
+import os
+from pathlib import Path
+
+import aiohttp
 from langchain.tools import  tool
 from langchain.document_loaders import PyPDFLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain.embeddings import HuggingFaceEmbeddings
 from langchain.vectorstores import Chroma
+from Agent.Tools.Article import Article
+from pydantic import SecretStr
 
+
+# Constants and Configuration
+GOOGLE_API_KEY = SecretStr(os.environ["GOOGLE_API_KEY"])
+SERPAPI_API_KEY = SecretStr(os.environ["SERPAPI_API_KEY"])
 
 @tool
-def read_pdf_and_save():
+def read_pdf_and_save(path: Path) -> None:
     #pdf yükleme işlemi
-    loader = PyPDFLoader('docs/example.pdf')
+    loader = PyPDFLoader(path)
     document = loader.load()
 
     #pdfi chunklara bölüyoruz verim için.
@@ -26,3 +36,19 @@ def read_pdf_and_save():
     )
     #Kalıcı olarak saklıyoruz
     vectorstore.persist()
+
+
+@tool
+async def serp_api_search(query: str) -> list[Article]:
+    params = {
+        'api_key': SERPAPI_API_KEY.get_secret_value(),
+        'engine': 'google',
+        'q': query
+    }
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            "https://serpapi.com/search",
+            params=params
+        ) as response:
+            results = await response.json()
+    return [Article.from_serpapi_result(result) for result in results['organic_results']]
